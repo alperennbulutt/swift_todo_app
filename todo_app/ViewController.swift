389afd193abfccc56389e808eae84ed50b302131
alperennbulutt <alperenbulut.ab@gmail.com>
2022-12-11 01:25:06 +0300

    .

--- todo_app/ViewController.swift
+++ todo_app/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var metallicaLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var array = [String]()
 
+    
+    
+    
+    // uygulama ilk yüklenirken çalışan fonksiyon
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeButtonOnTap(_ sender: Any) {
+        
+        array.append("alperen")
+        
+    
+        
+    }
+    
+    @IBAction func printArrayButtonOnTap(_ sender: Any) {
+        print(array)
+    }
+    
+    
+    
+    
+    
 }
 
